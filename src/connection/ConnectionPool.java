bd57aed4b663172fb161e2db4b07b16e9c78ad51
wenxiaoyang <xiaoyang.wem@arvato.com>
2019-06-17 10:07:14 +0800

    java并发编程艺术，实例代码

---
+++ src/connection/ConnectionPool.java
@@ -0,0 +1,55 @@
+package connection;
+
+import java.sql.Connection;
+import java.util.LinkedList;
+
+/**
+ * @author xiaoyang.wen
+ * @date 2019/6/14 16:49
+ */
+public class ConnectionPool {
+
+    private LinkedList<Connection> pool = new LinkedList<>();
+
+
+    public ConnectionPool(int initialSize) {
+        if (initialSize > 0) {
+            for (int i = 0; i < initialSize; i++) {
+                pool.addLast(ConnectionDriver.createConnection());
+            }
+        }
+    }
+
+    public void releaseConnection(Connection connection) {
+        if (connection != null) {
+            synchronized (pool) {
+                pool.addLast(connection);
+                pool.notifyAll();
+            }
+        }
+    }
+
+    public Connection fetchConnection(long mills) throws InterruptedException {
+        synchronized (pool) {
+            if (mills <= 0) {
+                //没有连接实例一直等待
+                while (pool.isEmpty()) {
+                    pool.wait();
+                }
+                return pool.removeFirst();
+            } else {
+                long future = System.currentTimeMillis() + mills;
+                long remaining = mills;
+                while (pool.isEmpty() && remaining > 0) {
+                    pool.wait(remaining);
+                    remaining = System.currentTimeMillis() - future;
+                }
+                Connection result = null;
+                if (!pool.isEmpty()) {
+                    result = pool.removeFirst();
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ src/connection/ConnectionPoolTest.java
@@ -0,0 +1,11 @@
+package connection;
+
+/**
+ * @author xiaoyang.wen
+ * @date 2019/6/14 17:23
+ */
+public class ConnectionPoolTest {
+
+
+
+}
