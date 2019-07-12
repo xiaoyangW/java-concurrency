63af7bc005cec68936adae26c1e92290ff2c3198
XiaoYang.Wen <xiaoyang.wen@arvato.com>
2019-07-12 14:32:58 +0800

    condition

--- src/lock/ConditionUseCase.java
+++ src/lock/ConditionUseCase.java
@@ -10,8 +10,8 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ConditionUseCase {
 
-    Lock  lock = new ReentrantLock();
-    Condition condition = lock.newCondition();
+    private Lock  lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
 
     public void conditionWait() throws InterruptedException {
         lock.lock();
